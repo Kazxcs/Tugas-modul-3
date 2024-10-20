d60d35f9d1b339acc847d6f21d3e8391929ffb69
Kazxcs <155834336+Kazxcs@users.noreply.github.com>
2024-10-20 19:59:12 +0700

    Add files via upload

---
+++ soal3.2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func f(x int) int {
+	return x * x
+}
+
+func g(x int) int {
+	return x - 2
+}
+
+func h(x int) int {
+	return x + 1
+}
+
+func fogoh(x int) int {
+	return f(g(h(x)))
+}
+
+func gohof(x int) int {
+	return g(h(f(x)))
+}
+
+func hofog(x int) int {
+	return h(f(g(x)))
+}
+
+func main() {
+	var a, b, c int
+
+	fmt.Println("Masukkan tiga bilangan:")
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+
+	resultFogoh := fogoh(a)
+	resultGohof := gohof(b)
+	resultHofog := hofog(c)
+
+	fmt.Println(resultFogoh)
+	fmt.Println(resultGohof)
+	fmt.Println(resultHofog)
+}
